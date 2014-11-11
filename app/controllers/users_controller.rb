e95c2c4d6fd9f70892edaa121aa95ce53f49d425
Kristel Teng <kristelteng@gmail.com>
2014-11-11 09:39:06 -0500

    Creating log in functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to products_url, notice: "Signed up!"
+      redirect_to products_url, {notice: "Signed up!"}
     else
       render "new"
   end
@@ -16,6 +16,6 @@ end
   private
 
   def user_params
-    params.require(:user).permit(:email, :password. :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   get 'users/new'
 
   get 'users/create'
@@ -21,6 +27,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
     resources :products
     resources :users, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
 
   # Example resource route with options:
   #   resources :products do
