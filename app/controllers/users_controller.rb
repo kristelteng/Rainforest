318a62793eb87075e384b3fd1966200ea573d9a2
Kristel Teng <kristelteng@gmail.com>
2014-11-11 19:59:45 -0500

    Adding user names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to products_url, {notice: "Signed up!"}
     else
       render "new"
@@ -16,6 +17,6 @@ end
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end 
