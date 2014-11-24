1f0244f31659662a847f7f435535216674eacb54
Kristel Teng <kristelteng@gmail.com>
2014-11-24 18:13:01 -0500

    Using the XHR object

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,17 @@
 class ProductsController < ApplicationController
+  
   def index
     @products = if params[:search]
       Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
     else
       Product.all
     end
-    render :index
+    
+    if request.xhr?
+      render @products
+    end
   end
     
-  
-
   def show
     @product = Product.find(params[:id])
     if current_user
