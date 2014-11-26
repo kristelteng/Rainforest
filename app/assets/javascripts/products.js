11ffacbffdef375a1ce81205e7d84e9db1e28d0a
Kristel Teng <kristelteng@gmail.com>
2014-11-26 17:41:39 -0500

    Fixed missing paren, and started searching + pagination

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,11 +1,21 @@
+
 $(document).on("ready page:load", function() {
-
-  $("#search-form").submit(function(event) {
+  $('search-form').submit(function(event) {
     event.preventDefault();
-    var searchValue = $("#search").val();
+    var searchValue = $('search').val();
 
-  $.getScript("/products?search=" + searchValue);
-    
+    $.getScript('/products?search=' + searchValue);
+  
 
   });
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination span.next').children().attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Fetching more products..."); 
+        return $.getScript(url);
+      }
+    });
+  };
 });
+
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,11 @@ class ProductsController < ApplicationController
     @products = if params[:search]
       Product.where("LOWER(name) LIKE LOWER(?)", "%#{params[:search]}%")
     else
-      Product.all
+      Product.order("products.created_at DESC")
     end
 
+    @products = @products.page(params[:page])
+    
     respond_to do |format|
       format.html
       format.js
